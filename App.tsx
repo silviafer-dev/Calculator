084ea5f1ec6cb0bada446cb76a12bd8562432adb
Silvia Ferrai <silviafer@hotmail.it>
2023-01-04 11:21:04 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {CalculatorScreen} from './src/screens/CalculatorScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <CalculatorScreen />
+    </SafeAreaView>
+  );
+};
+export default App;
---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text>Calculator</Text>
+    </View>
+  );
+};
